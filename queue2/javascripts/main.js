751d223e7aed3020157b26d1093e9efc4265d1d0
三橋啓多 <keita.mitsuhashi.83@gmail.com>
2016-08-14 16:59:02 +0900

    add queue2

--- queue2/javascripts/main.js
+++ queue2/javascripts/main.js
@@ -9,5 +9,20 @@ $(function() {
     $(content_id).addClass(selectedClassStr);
     $('#content_wrap').find('.content').fadeOut(1000);
     $(content_id).fadeIn(1000);
+
+    var ch = $(content_id).height() + 200;
+    var sh = $('#square').height();
+    var diff = ch - sh;
+    var dist = '+=' + diff;
+    console.log(diff);
+
+    $("#square").animate({
+      top:dist,
+      height: ch
+    }, 1000);
+
+    $("#greeting").animate({
+      top: dist
+    }, 2000);
   });
 });
