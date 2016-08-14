98f05e43c87e3c4fc1aa426b933cc83dd4718316
三橋啓多 <keita.mitsuhashi.83@gmail.com>
2016-08-14 16:28:38 +0900

    update queue dir

---
+++ queue1/javascripts/main.js
@@ -0,0 +1,13 @@
+$(function() {
+  $('#tmenu td').click(function(){
+    $('#hello').fadeOut();
+    var selectedClassStr = 'selected';
+    var content_id = '#content_' + this.id;
+    $('#tmenu').find('a').removeClass(selectedClassStr);
+    $('#content_wrap').find('.content').removeClass(selectedClassStr);
+    $(this).find('a').addClass(selectedClassStr);
+    $(content_id).addClass(selectedClassStr);
+    $('#content_wrap').find('.content').fadeOut(1000);
+    $(content_id).fadeIn(1000);
+  });
+});
